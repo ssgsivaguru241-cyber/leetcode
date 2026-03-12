b304a3c046051203d5b6ebd08e2ab9a4bed6efc3
ssgsivaguru241-cyber <ssgsivaguru241@gmail.com>
2026-03-12 10:17:34 +0530

    Time: 1 ms (96.3%), Space: 47 MB (88.18%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];   
+            ans[i + n] = nums[i];  
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        Solution sol = new Solution();
+        int[] ans = sol.getConcatenation(nums);
+        System.out.println("Concatenated array: " + Arrays.toString(ans));
+    }
+}
