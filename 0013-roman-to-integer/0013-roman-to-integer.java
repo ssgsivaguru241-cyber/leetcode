cd239b24a68777649d33bd8384b6e42f7c97d223
ssgsivaguru241-cyber <ssgsivaguru241@gmail.com>
2026-03-12 10:23:19 +0530

    Time: 2 ms (99.75%), Space: 46.7 MB (49.01%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,58 @@
+class Solution {
+    static int value(char c)
+    {
+        if(c == 'I')
+        {
+            return 1;
+        }
+        if(c == 'V')
+        {
+            return 5;
+        }
+        if(c == 'X')
+        {
+            return 10;
+        }
+        if(c == 'L')
+        {
+            return 50;
+        }
+        if(c == 'C')
+        {
+            return 100;
+        }
+        if(c == 'D')
+        {
+            return 500;
+        }
+        if(c == 'M')
+        {
+            return 1000;
+        }
+        return -1;
+    }
+    public int romanToInt(String s) {
+        int res = 0;
+        int a = s.length();
+        for(int i=0;i<a;i++)
+        {
+            int s1 = value(s.charAt(i));
+            if(i+1<a)
+            {
+                int s2 = value(s.charAt(i+1));
+                if(s1>=s2)
+                {
+                    res += s1;
+                }
+                else{
+                    res+=(s2-s1);
+                    i++;
+                }
+            }
+            else{
+                res += s1;
+            }
+        }
+        return res;
+    }
+}
