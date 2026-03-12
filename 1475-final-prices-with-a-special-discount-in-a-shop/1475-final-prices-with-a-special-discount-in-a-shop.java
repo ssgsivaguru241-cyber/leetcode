2071337c7749996a2b579f851219952773e6197d
ssgsivaguru241-cyber <ssgsivaguru241@gmail.com>
2026-03-12 11:44:44 +0530

    Time: 4 ms (52.6%), Space: 45.3 MB (73.87%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,18 +1,36 @@
+// class Solution {
+//     public int[] finalPrices(int[] prices) {
+//         int n = prices.length;
+//         int[] arr = new int[n];
+//         for(int i = 0 ; i<n;i++){
+//             arr[i] = prices[i];
+//             for(int j = i+1;j<n;j++){
+//                 if(prices[j] <= prices[i])
+//                 {
+//                     arr[i] = prices[i] - prices[j];
+//                     break;
+//                 }
+//             }
+
+//         }
+//         return arr;
+//     }
+// }
 class Solution {
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
         int[] arr = new int[n];
+        Stack<Integer> st = new Stack<>();
         for(int i = 0 ; i<n;i++){
             arr[i] = prices[i];
-            for(int j = i+1;j<n;j++){
-                if(prices[j] <= prices[i])
-                {
-                    arr[i] = prices[i] - prices[j];
-                    break;
-                }
-            }
-
         }
+            for(int i = 0;i<n;i++){
+                while (!st.isEmpty() && prices[st.peek()] >= prices[i]) {
+                arr[st.pop()] -= prices[i];
+            }
+            st.push(i);
+        }
+
         return arr;
     }
 }
