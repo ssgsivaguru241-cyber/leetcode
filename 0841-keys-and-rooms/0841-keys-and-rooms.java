cfa21cbdb2c58441fd2af6a0310b7bef4b362910
ssgsivaguru241-cyber <ssgsivaguru241@gmail.com>
2026-03-26 12:16:27 +0530

    Time: 1 ms (85.76%), Space: 46 MB (94.02%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] vis = new boolean[rooms.size()];
+        dfs(0,rooms,vis);
+        for(boolean v:vis){
+            if(!v){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean dfs(int room,List<List<Integer>> rooms,boolean[] vis){
+        vis[room] = true;
+        for(int key:rooms.get(room)){
+            if(!vis[key]){
+                dfs(key,rooms,vis);
+            }
+        }
+        return false;
+    }
+}
